b0bd8b4eded290221e2853e1750712af48fcfab0
David Cargatser <cargatser@gmail.com>
2021-07-19 11:37:05 +0300

    remove fill from svgs

--- src/app/axios.ts
+++ src/app/axios.ts
@@ -1,9 +1,9 @@
 const axios = require('axios');
 export default axios.create({
-  baseURL: "http://cargatser.com/",
+  baseURL: "http://cargatser.com/api",
   timeout: 1000,
   headers: {
-    "Access-Control-Allow-Origin": "http://cargatser.com/", 
+    "Access-Control-Allow-Origin": "http://cargatser.com/api", 
     "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS"
   },
 });
