eef04636d1e5cfa24c9230ef32e78ff30078448b
David Cargatser <cargatser@gmail.com>
2021-07-20 14:04:22 +0300

    fix wrong dependencey array that force to reload data

--- src/app/axios.ts
+++ src/app/axios.ts
@@ -1,9 +1,9 @@
 const axios = require("axios");
 export default axios.create({
-  baseURL: "http://cargatser.com/api",
+  baseURL: "http://quotes.cargatser.com/api",
   timeout: 1000,
   headers: {
-    "Access-Control-Allow-Origin": "http://cargatser.com/api",
+    "Access-Control-Allow-Origin": "http://quotes.cargatser.com/api",
     "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
   },
 });
